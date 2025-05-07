1712d7e9d41eb1c3cfd00530d1b60db1bd4f4f0d
Admin <ulanapolancikova@gmail.com>
2025-05-07 13:32:27 +0300

    second

--- main_1.py
+++ main_1.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, redirect
+from forms.Loginform import LoginForm
+from forms.RegisterForm import RegisterForm
+from data.users import User
+from data import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
-
-# здесь будет база данных
-# db_session.global_init('mars_explorer.sqlite')
-# db_sess = db_session.create_session()
-# colonists = db_sess.query(User).all()
+db_session.global_init('db/users.sqlite')
 
 
 @app.route('/')
@@ -17,18 +16,58 @@ def home():
 
 
 @app.route('/login', methods=['GET', 'POST'])
-def registration():
-    return render_template('login.html')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        if not db_sess.query(User).filter(User.nickname == form.nickname.data).first():
+            return render_template('login.html', title='Регистрация',
+                                   form=form,
+                                   message="Такого пользователя нет")
+        if not db_sess.query(User).filter(User.password == form.password.data).first():
+            return render_template('login.html', title='Регистрация',
+                                   form=form,
+                                   message="Неверный пароль")
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
-@app.route('/registration')
+@app.route('/registration', methods=['GET', 'POST'])
 def login1():
-    return render_template('registration.html')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('registration.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.nickname == form.nickname.data).first():
+            return render_template('registration.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой никнейм уже есть")
+        elif db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('registration.html', title='Регистрация',
+                                   form=form,
+                                   message="Такая почта уже есть")
+        elif db_sess.query(User).filter(User.password == form.password.data).first():
+            return render_template('registration.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пароль уже есть")
+        user = User(
+            nickname=form.nickname.data,
+            name=form.name.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('registration.html', title='Регистрация', form=form)
 
 
 @app.route('/success')
 def main():
-    return render_template('index.html', title='Домашняя страница')
+    return render_template('shop.html', title='Домашняя страница')
 
 
 if __name__ == '__main__':
